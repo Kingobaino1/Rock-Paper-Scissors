b104f4ea8e4c7461018d3da971f5cfb710c9d306
Kingsley <kingobaino@gmail.com>
2020-06-04 10:37:21 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+let options = ['rock', 'paper', 'scissors'];
+let displays = document.querySelector('.display');
+let btn1 = document.querySelector('.buttons');
+let keys = document.querySelectorAll('button');
+let displayCount = document.querySelector('.com-count');
+let displayCounts = document.querySelector('.play-count');
+
+let computerCount = 0;
+let playerCount = 0;
+
+function play(computerSelection, playerSelection){
+    if(computerSelection === 'rock' && playerSelection === 'paper'|| computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock'){
+displays.textContent = `You win!!! Computer choose ${computerSelection}`
+playerCount ++
+    } else if(computerSelection === playerSelection){
+        displays.textContent = `Tie!!! Computer choose ${computerSelection}`
+    } else{
+        displays.textContent = `You Loose!!! Computer choose ${computerSelection}`
+        computerCount++
+    }
+}
+function computerPlay(){
+    let random = Math.floor(Math.random() * options.length);
+    computerSelection = options[random];
+}
+
+    function playRock(){
+        computerPlay()
+    let playerSelection = 'rock';
+    play(computerSelection, playerSelection);
+    displayCount.textContent = `Player Count is ${playerCount}`
+   displayCounts.textContent = `Computer Count is ${computerCount}`
+   fiveRounds();
+}
+
+function playPaper(){
+    computerPlay()
+    let playerSelection = 'paper';
+    play(computerSelection, playerSelection);
+   displayCount.textContent = `Player Count is ${playerCount}`
+   displayCounts.textContent = `Computer Count is ${computerCount}`
+   fiveRounds();
+}
+function playScissors(){
+    computerPlay()
+    let playerSelection = 'scissors';
+    play(computerSelection, playerSelection);
+    displayCount.textContent = `Player Count is ${playerCount}`
+   displayCounts.textContent = `Computer Count is ${computerCount}`
+   fiveRounds();
+}
+
+function reset(){
+location.reload()
+}
+keys.forEach((item) => {
+    if(item === keys[0]){
+        item.addEventListener('click', playRock)
+    } else if(item === keys[1]){
+        item.addEventListener('click', playPaper)
+
+    }  else if(item === keys[2]){
+        item.addEventListener('click', playScissors)
+    } else{
+        item.addEventListener('click', reset)
+
+    }
+
+})
+function fiveRounds(){
+    if(playerCount + computerCount === 5){
+        if(playerCount > computerCount){
+            displays.textContent = `Congratulations!!! You win.`
+            displays.style.cssText = 'color: green; font-size: 40px';
+        }else if(playerCount < computerCount){
+            displays.textContent = `Oooops!!! Computer Wins.`
+            displays.style.cssText = 'color: red; font-size: 40px';
+        }
+    } else if(playerCount + computerCount >= 5){
+        location.reload();
+    }
+}
